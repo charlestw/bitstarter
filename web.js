b9a9a2a3a790f0addefecb4f81889d72139ccce3
Ubuntu <ubuntu@ip-172-31-22-236.us-west-2.compute.internal>
2013-07-07 02:32:42 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buf = new Buffer(256);
+var buf = new Buffer("Hello World from index.html",25);
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html","rtf-8"));
