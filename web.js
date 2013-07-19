6730d9ccfa3ff09f6c767164f3889b096529351c
Charles Wu <tw.mooc@gmail.com>
2013-07-19 02:39:12 +0000

    Added/Modified files for A4 P2

--- web.js
+++ web.js
@@ -4,11 +4,13 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 var out = fs.readFileSync("index.html","utf-8");
 
+app.use(express.static(__dirname + '/img'));
+
 app.get('/', function(request, response) {
   response.send(out);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
